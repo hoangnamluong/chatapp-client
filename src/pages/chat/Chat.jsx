018712d65d77ba3b9920496e7d524d7d0ba5ba8b
lhn <hoangnamluong.it.production@gmail.com>
2023-04-12 14:38:24 +0700

    v5

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -2,6 +2,7 @@ import "./chat.scss";
 import ChatsList from "../../components/Chat/ChatsList";
 import UserSearchDrawer from "../../components/User/UserSearchDrawer";
 import GroupChatModal from "../../components/GroupChat/GroupChatModal";
+import GroupForm from "../../components/GroupChat/GroupForm";
 import ChatBox from "../../components/Chat/ChatBox";
 import ChatAction from "../../components/Chat/ChatAction";
 
